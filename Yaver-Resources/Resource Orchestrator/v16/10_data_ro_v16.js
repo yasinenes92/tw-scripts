804f36cd3d56ba41a2665e9e4346aa979492eb17
yasinenes92 <saatsabahin5i@gmail.com>
2026-02-18 20:35:38 +0300

    v16

---
+++ Yaver-Resources/Resource Orchestrator/v16/10_data_ro_v16.js
@@ -0,0 +1,22 @@
+(function () {
+  'use strict';
+
+  var Y = window.YRO_V16;
+  if (!Y) return;
+
+  Y.data = {
+    GROUP_SELECTORS: {
+      groupSelect:
+        'select[name="group_id"], select#group_id, select[name="group"], select[name="group_id[]"]',
+      groupMenuItems: '.group-menu-item[data-group-id], strong.group-menu-item[data-group-id]',
+      villageIdSpans: 'span.quickedit-vn[data-id]',
+    },
+
+    MARKET_SELECTORS: {
+      merchAvail: '#market_merchant_available_count',
+      merchTotal: '#market_merchant_total_count',
+    },
+  };
+
+  Y.log('data module loaded ✅');
+})();
